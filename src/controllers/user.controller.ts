8562ed452e34bd36f82f8659299261a7a4ee102a
Kasyanovskaya Kristina <kristinochkakv@gmail.com>
2024-03-05 17:21:36 +0300

    feat: add user

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { User } from 'src/interface/interface';
+import { UserService } from 'src/services/user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
+  }
+}
---
+++ src/services/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/interface/interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UserService {
+  private readonly users: User[] = [
+    {
+      id: '1',
+      login: 'User 1',
+      password: 'password1',
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+    {
+      id: '2',
+      login: 'User 2',
+      password: 'password2',
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+    {
+      id: '3',
+      login: 'User 3',
+      password: 'password3',
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+  ];
+
+  // Добавила нового пользователя
+  async addUser(login: string, password: string): Promise<User> {
+    const newUser: User = {
+      id: uuidv4(),
+      login,
+      password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.users;
+  }
+}
